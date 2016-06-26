5877d1af51403ea77926c63b0d7db229caac2b1f
Roman Tumaykin <roman@tumaykin.com>
2016-06-25 23:11:14 -0700

    Added a calculator sample

---
+++ src/examples/calculator/Calculator.Extension.Additor/Additor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator.Extension
+{
+    public class Additor : ICalculator
+    {
+        public int Calculate(int a, int b)
+        {
+            // this line shows that it is referencing shared type from its local directory
+            Console.WriteLine($"Additor SharedType Runtime Version: {typeof(SharedType).Assembly.ImageRuntimeVersion}. Codebase: {typeof(SharedType).Assembly.CodeBase}.");
+            return a + b;
+        }
+    }
+}
---
+++ src/examples/calculator/Calculator/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Calculator.Extension;
+using Impromptu;
+using Impromptu.AssemblyResolver;
+
+namespace SharedTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (AppDomain.CurrentDomain.IsDefaultAppDomain())
+            {
+                var appDomainSetup = AppDomain.CurrentDomain.SetupInformation;
+
+                appDomainSetup.DisallowApplicationBaseProbing = true;
+                var workerDomain = AppDomain.CreateDomain("Worker Domain", null, appDomainSetup);
+                workerDomain.ExecuteAssembly(typeof(Program).Assembly.Location);
+            }
+            else
+            {
+                DefaultContext.WireUpResolver();
+
+                var config = AppDomain.CurrentDomain.BaseDirectory.Split('\\').Last(s => !string.IsNullOrWhiteSpace(s));
+
+                const string additorRelativePath = "..\\..\\..\\Calculator.Extension.Additor.NugetPackage\\bin\\";
+                const string subtractorRelativePath = "..\\..\\..\\Calculator.Extension.Subtractor.NugetPackage\\bin\\";
+
+                var nugetPackageRetriever =
+                    new Impromptu.Package.NugetPackageRetriever(new[]
+                    {
+                        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, additorRelativePath, config),
+                        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, subtractorRelativePath, config)
+                    });
+
+                // cleanup
+                var additorFolderPath =
+                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "ImpromptuPackages", "Calculator.Extension.Additor.1.0.0");
+
+                var subtractorFolderPath =
+                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "ImpromptuPackages", "Calculator.Extension.Subtractor.1.0.0");
+
+                // cleanup
+                if (Directory.Exists(additorFolderPath))
+                    Directory.Delete(additorFolderPath, true);
+
+                if (Directory.Exists(subtractorFolderPath))
+                    Directory.Delete(subtractorFolderPath, true);
+
+                var factory = new InstantiatorFactory<ICalculator>(nugetPackageRetriever);
+                // this line shows that it is referencing shared type from its local directory
+                Console.WriteLine(
+                    $"Main Program SharedType Runtime Version: {typeof(SharedType).Assembly.ImageRuntimeVersion}. Codebase: {typeof(SharedType).Assembly.CodeBase}.");
+
+                var additionResult =
+                    factory.Instantiate(new InstantiatorKey("Calculator.Extension.Additor", "1.0.0", "Calculator.Extension.Additor"))
+                        .Calculate(10, 5);
+                Console.WriteLine($"Addition Result = {additionResult}");
+
+                var subtractionResult =
+                    factory.Instantiate(new InstantiatorKey("Calculator.Extension.Subtractor", "1.0.0", "Calculator.Extension.Subtractor"))
+                        .Calculate(10, 5);
+                Console.WriteLine($"Subtraction Result = {subtractionResult}");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
